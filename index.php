d05c0dc4196cb65c1a105d130df89b4aadf238cb
anhquanqueo25 <aqdz01@gmail.com>
2021-09-14 08:53:20 +0700

    them web food

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Categories</title>
+    <link link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+  
+    <header>
+        <a href="https://www.facebook.com/quanqueo25/" class="logo"><img src="https://scontent.fhan4-3.fna.fbcdn.net/v/t1.15752-9/140939784_439964713857728_8942879860414551367_n.png?_nc_cat=103&ccb=1-5&_nc_sid=ae9488&_nc_ohc=xEdOdD6KhHsAX8OuyRK&_nc_ht=scontent.fhan4-3.fna&oh=c75aa10b3a79f35db6a903e268cde8ed&oe=6165DAC7"> </a>
+        <div class="luachon">
+         <nav class="navbar">
+            <a class="a1" href="#home">Home</a>
+            <a class="a2" href="speciality">Categories</a>
+            <a class="a3" href="popular">Foods</a>
+            <a class="a4" href="gallery">Contact</a>
+          </nav>
+        </div>
+        
+    </header>
+      <h1 class="noidung">Explore Foods</h1>
+     <main>
+       <div class="hang1">
+        <div class="hinhngang1">
+          <a href=""><img src="image/piza.jpg"></a>
+           <p class="box1">Piza</p>  
+        </div>   
+        <div class="hinhngang2">
+           <a href=""><img src="image/burger.jpg"></a> 
+           <p class="box2">Burger</p> 
+        </div>
+        <div class="hinhngang3">
+           <a href=""><img src="image/momo.jpg"></a> 
+           <p class="box3">Momo</p> 
+        </div>
+      </div> 
+      <div class="hang2">
+        <div class="hinhngang21">
+          <a href=""><img src="image/piza.jpg"></a>
+           <p class="box4">Piza</p>  
+        </div>   
+        <div class="hinhngang22">
+           <a href=""><img src="image/burger.jpg"></a> 
+           <p class="box5">Burger</p> 
+        </div>
+        <div class="hinhngang23">
+           <a href=""><img src="image/momo.jpg"></a> 
+           <p class="box6">Momo</p> 
+        </div>
+      </div> 
+      <div class="hang3">
+        <div class="hinhngang31">
+          <a href=""><img src="image/piza.jpg"></a>
+           <p class="box7">Piza</p>  
+        </div>   
+        <div class="hinhngang32">
+           <a href=""><img src="image/burger.jpg"></a> 
+           <p class="box8">Burger</p> 
+        </div>
+        <div class="hinhngang33">
+           <a href=""><img src="image/momo.jpg"></a> 
+           <p class="box9">Momo</p> 
+        </div>
+      </div> 
+      <!-- <div class="infor">
+        <div class="fb">
+          <a href=""><img src="image/1200px-Facebook_Logo_(2019).png"></a>
+        </div>
+        <div class="is">
+          <a href=""><img src="image/Instagram_logo_2016.svg.png"></a>
+        </div>
+        <div class="tt">
+          <a href=""><img src="image/1259px-Twitter_bird_logo_2012.svg.png"></a>
+        </div>
+        
+      </div> -->
+       <div class="headerbt">All rights reserved.Designed By<a href="http://cse.tlu.edu.vn/">CSE.TLU</a></div>
+    </main>  
+    
+    
+  
+</body>
+</html>
